07fc4ac20aac139bcfd4570b4910a10e25a521e4
Alexey Letunov <xnihonsamuraix@gmail.com>
2023-12-15 10:04:05 +0300

    init

---
+++ EntityManagerFactoryUsing/src/main/java/example/JndiDataSiurceCfg.java
@@ -0,0 +1,20 @@
+package example;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+
+@Configuration
+public class JndiDataSiurceCfg 
+{
+	@Bean
+	public DataSource dataSource()
+	{
+		final JndiDataSourceLookup dsLookup = 
+				new JndiDataSourceLookup();
+		dsLookup.setResourceRef(true);
+		return dsLookup.getDataSource("persistence/")
+	}
+}
